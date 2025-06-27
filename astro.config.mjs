64d629820d6cd334dedc8da2bb30f9ce4460bed9
teij <teij.working@gmail.com>
2025-06-27 22:47:52 +0900

    Configure Netlify adapter manually

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify"; // netlifyをインポート
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: 'server', // Netlifyアダプターにはこの設定が必要です
+  integrations: [
+    tailwind(),
+    netlify() // integrationsにnetlifyを追加
+  ]
 });
